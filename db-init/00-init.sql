7ab910c14bf1c67c1daf361ae1bd430c13388b5b
tiboitel <jules.boitelle@gmail.com>
2026-04-14 04:46:05 +0200

    fix: remove localhost grant from init SQL (caused init failure)
    - Remove GRANT for 'starloco'@'localhost' which doesn't exist when MariaDB
      creates user via MARIADB_USER/MARIADB_PASSWORD env vars
    - Only grant to 'starloco'@'%' which is created by the official image

--- db-init/00-init.sql
+++ db-init/00-init.sql
@@ -5,9 +5,7 @@
 -- Create game database if not exists
 CREATE DATABASE IF NOT EXISTS starloco_game CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Create starloco user if not exists (using ALTER to ensure password is set correctly)
--- The user is created by MARIADB_USER/MARIADB_PASSWORD env vars, but we grant explicit privileges here
+-- Grant privileges to the starloco user (created by MARIADB_USER/MARIADB_PASSWORD env vars)
 GRANT ALL PRIVILEGES ON starloco_game.* TO 'starloco'@'%';
-GRANT ALL PRIVILEGES ON starloco_game.* TO 'starloco'@'localhost';
 
 FLUSH PRIVILEGES;
